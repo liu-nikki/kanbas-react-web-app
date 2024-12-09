c7f39211ec0246296010a2c396b04abb30c218c5
liu-nikki <108967384+liu-nikki@users.noreply.github.com>
2024-12-08 17:56:11 -0800

    a6 MongoDB

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,30 +1,26 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 export default function AccountNavigation() {
-  const location = useLocation();
-  const isActive = (path: any) => location.pathname === path;
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-  return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link 
-        to="/Kanbas/Account/Signin"
-        id="wd-account-signin"
-        className={`list-group-item border border-0 ${isActive('/Kanbas/Account/Signin') ? 'active' : 'text-danger'}`}>
-        Signin
-      </Link>
-      <Link 
-        to="/Kanbas/Account/Signup"
-        id="wd-account-signup"
-        className={`list-group-item border border-0 ${isActive('/Kanbas/Account/Signup') ? 'active' : 'text-danger'}`}>
-        Signup
-      </Link>
-      <Link 
-        to="/Kanbas/Account/Profile"
-        id="wd-account-Profile"
-        className={`list-group-item border border-0 ${isActive('/Kanbas/Account/Profile') ? 'active' : 'text-danger'}`}>
-        Profile
-      </Link>
-    </div>
-  );
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    const { pathname } = useLocation();
+    return (
+        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+            {!currentUser ? (
+                <>
+                    <NavLink id="wd-account-profile-link" to="/Kanbas/Account/Signin" className={({ isActive }) => "list-group-item border border-0" + (isActive ? "list-group-item active border border-0" : "list-group-item text-danger border border-0")}>Signin
+                    </NavLink>
+                    <NavLink id="wd-account-profile-link" to="/Kanbas/Account/Signup" className={({ isActive }) => "list-group-item border border-0" + (isActive ? "list-group-item active border border-0" : "list-group-item text-danger border border-0")}>Signup
+                    </NavLink>
+                </>
+            ) : (
+                <NavLink id="wd-account-profile-link" to="/Kanbas/Account/Profile" className={({ isActive }) => "list-group-item border border-0" + (isActive ? "list-group-item active border border-0" : "list-group-item text-danger border border-0")}>Profile
+                </NavLink>
+            )}
+{currentUser && currentUser.role === "ADMIN" && (
+      <Link to={`/Kanbas/Account/Users`} className={`list-group-item border border-0
+        ${pathname.includes("Users") ? "active" : "text-danger"}`}> Users </Link> )}
+        </div>
+    );
 }
